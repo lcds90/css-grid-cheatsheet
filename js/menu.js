cd9b039876349f6e3caec786ff415dd68dd806c5
lcds90 <lcds90@gmail.com>
2022-06-11 18:38:31 -0300

    feat: grid description ✍️

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,6 @@
 (function () {
   const select = document.querySelector('#menu');
-  const h1 = document.querySelector('#title');
   const nav = document.querySelector('.containerSummaryList');
-  h1.textContent = 'CSS Cheatsheet';
   nav.style.overflowY = 'hidden';
   const classname = 'containerSummaryList-on';
 
@@ -14,12 +12,10 @@
 
   nav.addEventListener('mouseleave', () => {
     nav.classList.remove(classname);
-    h1.textContent = 'Select a section';
     nav.style.overflowY = 'hidden';
   });
   nav.addEventListener('click', () => {
     nav.classList.add(classname);
-    h1.textContent = 'Welcome to CSS Cheatsheet';
     nav.style.overflowY = 'auto';
   });
 })();
